171ea1768f91951b867d600a30d5e8aeab9828f6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-25 19:00:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Route, Router, Routes } from "react-router";
+import { Toaster } from "sonner";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Toaster richColors position="top-right" />
+      <Suspense
+        fallback={
+          <p className="w-screen h-screen flex items-center justify-center text-2xl font-semibold text-center">
+            Loading...
+          </p>
+        }
+      >
+        <Router>
+          <Routes>
+            <Route></Route>
+          </Routes>
+        </Router>
+      </Suspense>
+    </>
+  );
 }
